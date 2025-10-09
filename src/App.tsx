7a17853445d03bce409be93d8485ed0b511dbcaa
ars8590 <perumballiarshak@gmail.com>
2025-10-09 11:07:52 +0530

    nas update by v2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Booth from "./pages/Booth";
 import Feed from "./pages/Feed";
 import Slideshow from "./pages/Slideshow";
@@ -13,22 +13,33 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+const AppContent = () => {
+  const location = useLocation();
+  const isSlideshow = location.pathname === "/slideshow";
+
+  return (
+    <>
+      {!isSlideshow && <Navigation />}
+      <Routes>
+        <Route path="/" element={<Booth />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/slideshow" element={<Slideshow />} />
+        <Route path="/admin" element={<Admin />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {!isSlideshow && <Footer />}
+    </>
+  );
+};
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Booth />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/slideshow" element={<Slideshow />} />
-          <Route path="/admin" element={<Admin />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <AppContent />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
